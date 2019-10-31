00fb14bc2a8a1e56644ad0fc32faebfb2f4de509
Juliano Souza <46322485+thespamer@users.noreply.github.com>
2019-10-31 00:14:06 -0300

    Create ec2.tf

---
+++ terraform-code/ec2.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-1"
+}
+
+resource "aws_instance" "asdojl" {
+  ami           = "ami-02df9ea15c1778c9c"
+  instance_type = "t2.micro"
+  key_name      = "ir1"
+  tags = {
+    Name = "asdojl"
+  }
+}
+
+resource "aws_eip" "asdojl" {
+  vpc      = true
+  instance = aws_instance.asdojl.id
+}
+
+resource "aws_s3_bucket" "asdojl" {
+  bucket = "tfasdojl"
+  acl    = "private"
+}
